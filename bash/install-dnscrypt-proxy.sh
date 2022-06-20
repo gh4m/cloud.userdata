86f915b36e58e59ac573c198d1343e36e1afae4e
Mark <gh4m@users.noreply.github.com>
2022-06-20 13:47:42 -0400

    fix dnscrypt-proxt path var

--- bash/install-dnscrypt-proxy.sh
+++ bash/install-dnscrypt-proxy.sh
@@ -9,7 +9,8 @@
 SET_AD_BLOCKING=$1
 DNSCRYPT_PROXY_VER=2.1.1  ## https://github.com/DNSCrypt/dnscrypt-proxy/releases/latest
 DNSCRYPT_PROXY_PATH=/opt/dnscrypt-proxy
-DNSCRYPT_PROXY_TOML_FILE=${DNSCRYPT_PROXY_PATH}/dnscrypt-proxy.toml
+DNSCRYPT_PROXY_TOML_FILE_NAME=dnscrypt-proxy.toml
+DNSCRYPT_PROXY_TOML_FILE_PATH=${DNSCRYPT_PROXY_PATH}/${DNSCRYPT_PROXY_TOML_FILE_NAME}
 
 mkdir ${DNSCRYPT_PROXY_PATH}
 cd ${DNSCRYPT_PROXY_PATH}
@@ -25,13 +26,13 @@ tar xvf dnscrypt-proxy-linux_x86_64-${DNSCRYPT_PROXY_VER}.tar.gz
 mv linux-x86_64/* .
 rmdir linux-x86_64/
 
-cp example-${DNSCRYPT_PROXY_TOML_FILE} ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/listen_addresses = \['127.0.0.1:53'\]/c\listen_addresses = \['0.0.0.0:53'\]" ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/require_dnssec =/c\require_dnssec = true" ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/log_level = 2/c\log_level = 2" ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/file = 'query.log'/c\file = 'query.log'" ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/file = 'nx.log'/c\file = 'nx.log'" ${DNSCRYPT_PROXY_TOML_FILE}
-sed -i "/log_file = 'dnscrypt-proxy.log'/c\log_file = 'dnscrypt-proxy.log'" ${DNSCRYPT_PROXY_TOML_FILE}
+cp ${DNSCRYPT_PROXY_PATH}/example-${DNSCRYPT_PROXY_TOML_FILE_NAME} ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/listen_addresses = \['127.0.0.1:53'\]/c\listen_addresses = \['0.0.0.0:53'\]" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/require_dnssec =/c\require_dnssec = true" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/log_level = 2/c\log_level = 2" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/file = 'query.log'/c\file = 'query.log'" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/file = 'nx.log'/c\file = 'nx.log'" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+sed -i "/log_file = 'dnscrypt-proxy.log'/c\log_file = 'dnscrypt-proxy.log'" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
 
 if [ "${SET_AD_BLOCKING}" == "YES" ]
 then
@@ -42,8 +43,8 @@ then
   echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" >> ${DNSCRYPT_PROXY_BLOCKLIST_DOMAIN_CONF}
   ## For automated background updates, the script can be run as a cron job
   (crontab -l 2>/dev/null; echo "25 4 * * * python3 ${DNSCRYPT_PROXY_BLOCKLIST_SCRIPT} -c ${DNSCRYPT_PROXY_BLOCKLIST_DOMAIN_CONF} -o ${DNSCRYPT_PROXY_PATH}/blocked-names.txt") | crontab -
-  sed -i "/blocked_names_file =/c\blocked_names_file = 'blocked-names.txt'" ${DNSCRYPT_PROXY_TOML_FILE}
-  sed -i "/log_file = 'blocked-names.log'/c\log_file = 'blocked-names.log'" ${DNSCRYPT_PROXY_TOML_FILE}
+  sed -i "/blocked_names_file =/c\blocked_names_file = 'blocked-names.txt'" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
+  sed -i "/log_file = 'blocked-names.log'/c\log_file = 'blocked-names.log'" ${DNSCRYPT_PROXY_TOML_FILE_PATH}
 fi
 
 ./dnscrypt-proxy -service install
