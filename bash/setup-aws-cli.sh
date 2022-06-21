b7f88e6a98741b262a4500cb0392cf95452c01dd
Mark <gh4m@users.noreply.github.com>
2022-06-20 22:20:32 -0400

    updates

--- bash/setup-aws-cli.sh
+++ bash/setup-aws-cli.sh
@@ -10,5 +10,5 @@ $APT_GET_CMD -qq update
 dpkg -s unzip   &> /dev/null || $APT_GET_CMD install unzip
 uname -a | grep x86_64  && ( ls awscliv2.zip &> /dev/null || curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" )
 uname -a | grep aarch64 && ( ls awscliv2.zip &> /dev/null || curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" )
-ls -d ./aws/    &> /dev/null || unzip awscliv2.zip
+ls -d ./aws/    &> /dev/null || unzip -q awscliv2.zip
 aws --version   &> /dev/null || ./aws/install
