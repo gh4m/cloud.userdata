fe62750f48026923a012dfe3e18efeae6da22bf6
Mark <gh4m@users.noreply.github.com>
2022-06-20 22:33:41 -0400

    updates

--- bash/setup-wireguard-client.sh
+++ bash/setup-wireguard-client.sh
@@ -8,14 +8,15 @@ echo USERDATA_RUNNING $0 ${*}
 
 WG_CLIENT_PUB_KEY=$1
 WG_CLIENT_IP=$2
+WC_CLIENT_SHARE_FILE=/etc/wireguard/sharekey.$(dirname ${WG_CLIENT_IP})
 
 cd /etc/wireguard/
-umask 077 && wg genpsk > sharekey.${WG_CLIENT_IP}
+umask 077 && wg genpsk > ${WC_CLIENT_SHARE_FILE}
 
 cat << EOF >> /etc/wireguard/wg0.conf
 [Peer]
 PublicKey = ${WG_CLIENT_PUB_KEY}
-PresharedKey = $(cat /etc/wireguard/sharekey.${WG_CLIENT_IP})
+PresharedKey = $(cat ${WC_CLIENT_SHARE_FILE})
 AllowedIPs = ${WG_CLIENT_IP}
 
 EOF
