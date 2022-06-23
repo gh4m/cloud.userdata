1ab78c7e6b6350efc5ed796a5c9ca32f1bf0fea7
Mark <gh4m@users.noreply.github.com>
2022-06-22 22:58:24 -0400

    fix ufw dns port block;

--- bash/setup-ubuntu-server-firewall-wireguard.sh
+++ bash/setup-ubuntu-server-firewall-wireguard.sh
@@ -8,6 +8,7 @@ echo USERDATA_RUNNING $0 ${*}
 
 WG_NETWORK_VPN_CIDR=$1
 WG_SERVER_PORT=$2
+WG_SERVER_NIC_NAME=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
@@ -19,7 +20,7 @@ ufw allow proto tcp from ${WG_NETWORK_VPN_CIDR} to any port domain
 ufw allow proto udp from ${WG_NETWORK_VPN_CIDR} to any port domain
 ufw allow proto tcp from 0.0.0.0/0 to any port ssh
 ufw allow ${WG_SERVER_PORT}/udp
-ufw deny out 53/any
-ufw deny out 853/any
-ufw deny out 5353/any
+ufw deny out on ${WG_SERVER_NIC_NAME} to any port 53 proto any
+ufw deny out on ${WG_SERVER_NIC_NAME} to any port 853 proto any
+ufw deny out on ${WG_SERVER_NIC_NAME} to any port 5353 proto any
 ufw status verbose
