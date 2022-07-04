84206fae322dd619091885adcd13aa1d1ad42b20
Mark <gh4m@users.noreply.github.com>
2022-07-04 09:52:08 -0400

    comment adjusting

--- config/wireguard-postupdown.sh
+++ config/wireguard-postupdown.sh
@@ -97,8 +97,9 @@ then
 			ufw deny  out on ${WG_CLOUDVPN_ETH0_DEVICE_NAME} to any     port 5353 proto any
 		fi
 
-		## setup files for homeip cron script
+		## allow in ssh regardless of wg up or down
 		ufw allow proto tcp from 0.0.0.0/0 to any port ssh
+		## setup files for homeip adjust cron script
 		rm -f /var/tmp/home_cidr_previous_file.txt
 		rm -f /var/tmp/home__ip__previous_file.txt
 
@@ -112,6 +113,7 @@ then
 	##
 
 	WG_CLOUDVPN_ETH0_IP_ADDR=$(ip ad show dev ${WG_CLOUDVPN_ETH0_DEVICE_NAME} | grep "inet " | awk '{print $2}' | awk -F/ '{print $1}')
+	## packet in (source) from WG_CLOUDVPN_WGS1_NET_CIDR out through WG_CLOUDVPN_ETH0_DEVICE_NAME normal network to anywhere (no -d argument)
 	iptables -t nat ${IPTABLE_ACTION_FLAG} POSTROUTING -s ${WG_CLOUDVPN_WGS1_NET_CIDR} -o ${WG_CLOUDVPN_ETH0_DEVICE_NAME} -j SNAT --to-source ${WG_CLOUDVPN_ETH0_IP_ADDR}
 	
 fi
